b0d62c14d2720d34c32cfd283a09d16dda1e71cd
gplic <145370247+gplic@users.noreply.github.com>
2024-06-11 09:19:18 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+// LUMINENCE CALCULATOR
+
+// Event Listeners
+document.getElementById("calc").addEventListener("click", calculate);
+
+function calculate() {
+// Input
+    let R = +document.getElementById("red").value;
+    let G = +document.getElementById("green").value;
+    let B = +document.getElementById("blue").value;
+
+// Process
+    let brightness = Math.sqrt(0.299 * (R) ** 2 + 0.587 * (G) ** 2 + 0.114 * (B) ** 2);
+    let result = Math.round(brightness);
+// Output
+    document.getElementById("results").innerHTML = result;
+    document.getElementById("html").style.background = `rgb(${R}, ${G}, ${B})`
+}
